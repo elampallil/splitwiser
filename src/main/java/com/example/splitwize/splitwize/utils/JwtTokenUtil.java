2423699d471444915af69650a2a408cf0ec71d69
Nijo Jacob <nijo.jacob@gadgeon.com>
2020-07-19 13:08:13 +0530

    correct token utils

--- src/main/java/com/example/splitwize/splitwize/utils/JwtTokenUtil.java
+++ src/main/java/com/example/splitwize/splitwize/utils/JwtTokenUtil.java
@@ -5,33 +5,37 @@ import com.example.splitwize.splitwize.entity.UserRegiData;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-
 import java.io.Serializable;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+
 @Component
-@PropertySource(value = {"classpath:application.properties"})
+@PropertySource(value = { "classpath:application.properties" })
 public class JwtTokenUtil implements Serializable {
     private static final long serialVersionUID = -2550185165626007488L;
 
-    public static final long JWT_TOKEN_VALIDITY = 1;
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 1;
 
     @Value("${jwt.secret}")
     private String secret;
 
-    //retrieve username from jwt token
-    public String getIdFromToken(String token) {
+    // retrieve username from jwt token
+    public String getUsernameFromToken(String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-    //retrieve expiration date from jwt token
+    // retrieve expiration date from jwt token
     public Date getExpirationDateFromToken(String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
@@ -41,18 +45,18 @@ public class JwtTokenUtil implements Serializable {
         return claimsResolver.apply(claims);
     }
 
-    //for retrieveing any information from token we will need the secret key
+    // for retrieveing any information from token we will need the secret key
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
     }
 
-    //check if the token has expired
+    // check if the token has expired
     private Boolean isTokenExpired(String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
 
-    //generate token for user
+    // generate token for user
     public String generateToken(UserRegiData userRegiData) {
         Map<String, Object> claims = new HashMap<>();
         return doGenerateToken(claims, userRegiData.getId().toString());
@@ -60,14 +64,21 @@ public class JwtTokenUtil implements Serializable {
 
     private String doGenerateToken(Map<String, Object> claims, String subject) {
 
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration( new Date(Instant.now().toEpochMilli() + JWT_TOKEN_VALIDITY * 1000))
-                .signWith(SignatureAlgorithm.HS512, secret).compact();
+        return Jwts.builder()
+        .setClaims(claims)
+        .setSubject(subject)
+        .setIssuedAt(new Date(System.currentTimeMillis()))
+        .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY_SECONDS*1000))
+        .signWith(Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8)), SignatureAlgorithm.HS512)
+        .compact();
     }
 
-    //validate token
-    public Boolean validateToken(String token) {
-        final String username = getIdFromToken(token);
-        return (!isTokenExpired(token));
+    // validate token
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (
+              username.equals(userDetails.getUsername())
+                    && !isTokenExpired(token));
     }
+
 }
